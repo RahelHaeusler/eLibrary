f72d3743af00f0687c20190d5e02b3f8a7cbaac1
RahelHaeusler <125725850+RahelHaeusler@users.noreply.github.com>
2023-05-13 11:03:41 +0000

    Check in due to lack of internet connection

---
+++ crm-webservice/src/main/java/ch/fhnw/crm/crmwebservice/business/service/BookService.java
@@ -0,0 +1,9 @@
+
+
+@Service
+public class BookService {
+    
+    @Autowired
+    private BookRepository BookRepository;
+    
+}
---
+++ crm-webservice/src/main/java/ch/fhnw/crm/crmwebservice/data/domain/Book.java
@@ -0,0 +1,69 @@
+@Entity
+public class Book {
+    @Id
+    private int book_id;
+    private String ISBN;
+    private String title;
+    private int year;
+    private boolean borrowed;
+    private List<Author> authors;
+    private List<Genre> genres;
+}
+
+public void setBook_id(int book_id) {
+    this.book_id = book_id;
+}
+
+public int getBook_id() {
+    return book_id;
+}
+
+public void setISBN(String ISBN) {
+    this.ISBN = ISBN;
+}
+
+public String getISBN() {
+    return ISBN;
+}
+
+public void setTitle(String title) {
+    this.title = title;
+}
+
+public String getTitle() {
+    return title;
+}
+
+public void setYear(int year) {
+    this.year = year;
+}
+
+public int getYear() {
+    return year;
+}
+
+public void setBorrowed(boolean borrowed) {
+    this.borrowed = borrowed;
+}
+
+public boolean getBorrowed() {
+    return borrowed;
+}
+
+public void setAuthors(List<Author> authors) {
+    this.authors = authors;
+}
+
+public List<Author> getAuthors() {
+    return authors;
+}
+
+public void setGenres(List<Genre> genres) {
+    this.genres = genres;
+}
+
+public List<Genre> getGenres() {
+    return genres;
+}
+
+
